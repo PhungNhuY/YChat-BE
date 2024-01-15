6310e87d0fdcd626d7f64ddd2525ff9acc4f0a16
PhungNhuY <yphung282@gmail.com>
2024-01-15 22:51:54 +0700

    feat: env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import validationSchema from './configs/env.validation';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'development' ? '.env.dev' : '.env',
       expandVariables: true,
+      validationSchema: validationSchema,
     }),
   ],
   controllers: [AppController],
