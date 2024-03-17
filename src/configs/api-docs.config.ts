b7d9007a107503f811d1a47fa9943354e697c39f
PhungNhuY <yphung282@gmail.com>
2024-03-17 15:53:27 +0700

    feat: update swagger conifg

--- src/configs/api-docs.config.ts
+++ src/configs/api-docs.config.ts
@@ -3,6 +3,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 export function configSwagger(app: INestApplication) {
   const config = new DocumentBuilder()
     .setTitle('YChat-API documentation')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
