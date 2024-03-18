ab1e6bbcafc24aa951c9f578542f22bcaec2b4d1
PhungNhuY <yphung282@gmail.com>
2024-03-18 23:28:14 +0700

    feat: api get my profile

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthData } from 'src/decorators/auth-data.decorator';
+import { User } from './schemas/user.schema';
+import { UsersService } from './users.service';
+import {
+  buildSuccessResponse,
+  transformObjectToResponse,
+} from '@utils/api-response-builder.util';
+import { UserResponseDto } from './dtos/user-response.dto';
+import { JwtAccessTokenGuard } from 'src/guards/jwt-access-token.guard';
 
 @Controller('users')
-export class UsersController {}
+@UseGuards(JwtAccessTokenGuard)
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get('me')
+  async getMyProfile(@AuthData() user: User) {
+    const profile = await this.usersService.findOne(user._id.toString());
+    return buildSuccessResponse(
+      transformObjectToResponse(profile, UserResponseDto),
+    );
+  }
+}
