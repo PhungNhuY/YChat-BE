3bff10e1e2410eb7a5324342be85bb8fccd72d9f
PhungNhuY <yphung282@gmail.com>
2024-06-05 23:55:43 +0700

    feat: base transaction

---
+++ src/common/transaction.ts
@@ -0,0 +1,20 @@
+import { ClientSession, Connection } from 'mongoose';
+
+export async function transaction<T>(
+  connection: Connection,
+  callback: (session: ClientSession) => Promise<T>,
+): Promise<T> {
+  const session = await connection.startSession();
+
+  try {
+    session.startTransaction();
+    const result = await callback(session);
+    await session.commitTransaction();
+    return result;
+  } catch (error) {
+    await session.abortTransaction();
+    throw error;
+  } finally {
+    await session.endSession();
+  }
+}
