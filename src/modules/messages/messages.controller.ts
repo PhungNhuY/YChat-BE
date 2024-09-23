a7be6c824a3d4733d9d56d7e2e0618c244113093
PhungNhuY <yphung282@gmail.com>
2024-09-23 22:23:46 +0700

    feat: api get message, add pipe

--- src/modules/messages/messages.controller.ts
+++ src/modules/messages/messages.controller.ts
@@ -1,11 +1,25 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { JwtAccessTokenGuard } from 'src/guards/jwt-access-token.guard';
 import { UseAuthData } from 'src/decorators/use-auth-data.decorator';
 import { AuthData } from '@utils/types';
-import { buildSuccessResponse } from '@utils/api-response-builder.util';
+import {
+  buildSuccessResponse,
+  transformArrayToResponse,
+} from '@utils/api-response-builder.util';
 import { EventEmitter2 } from '@nestjs/event-emitter';
+import { ObjectIdValidationPipe } from 'src/pipes/objectid-validation.pipe';
+import { ApiQueryDto } from '@common/api-query.dto';
+import { MessageResponseDto } from './dtos/message-response.dto';
 
 @Controller('messages')
 @UseGuards(JwtAccessTokenGuard)
@@ -26,5 +40,25 @@ export class MessagesController {
     );
     this.eventEmitter.emit('message.new', newMessageData);
     return buildSuccessResponse();
+  }
+
+  @Get(':conversationId')
+  async findAll(
+    @Param('conversationId', new ObjectIdValidationPipe())
+    conversationId: string,
+    @Query() query: ApiQueryDto,
+    @UseAuthData() authData: AuthData,
+  ) {
+    const messages = await this.messageService.findAll(
+      conversationId,
+      query,
+      authData,
+    );
+    return buildSuccessResponse(
+      transformArrayToResponse(
+        { items: messages, total: messages.length },
+        MessageResponseDto,
+      ),
+    );
   }
 }
--- src/modules/messages/messages.service.ts
+++ src/modules/messages/messages.service.ts
@@ -5,6 +5,7 @@ import { Model } from 'mongoose';
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { AuthData, NewMessageData } from '@utils/types';
 import { Conversation } from '@modules/conversations/schemas/conversation.schema';
+import { ApiQueryDto } from '@common/api-query.dto';
 @Injectable()
 export class MessagesService {
   constructor(
@@ -39,5 +40,39 @@ export class MessagesService {
     });
 
     return { message: message.toObject(), conversation };
+  }
+
+  async findAll(
+    conversationId: string,
+    query: ApiQueryDto,
+    authData: AuthData,
+  ) {
+    // check valid conversation id
+    // user must be a conversation member
+    const validConversation = await this.conversationModel.exists({
+      deleted_at: null,
+      _id: conversationId,
+      members: {
+        $elemMatch: {
+          user: authData._id,
+        },
+      },
+    });
+    if (!validConversation)
+      throw new BadRequestException('Conversation not found');
+
+    const page = query.page || 1;
+    const limit = query.limit || 10;
+    const skip = (page - 1) * limit;
+    const messages = await this.messageModel
+      .find({
+        conversation: conversationId,
+        deleted_at: null,
+      })
+      .sort({ created_at: -1 })
+      .skip(skip)
+      .limit(limit)
+      .lean();
+    return messages;
   }
 }
