28aa4a2aef3e1b5493de5186bcd037a5c65d4afe
PhungNhuY <yphung282@gmail.com>
2024-09-30 22:41:59 +0700

    feat: update ws config

--- src/modules/websocket/websocket.adapter.ts
+++ src/modules/websocket/websocket.adapter.ts
@@ -15,25 +15,31 @@ export class WebSocketAdapter extends IoAdapter {
     super(app);
   }
   createIOServer(port: number, options?: ServerOptions) {
+    // port and cors configuration
     port = this.configService.get<number>('WS_PORT');
-    const server = super.createIOServer(port, options);
-
     const origins = JSON.parse(
       this.configService.get<string>('WS_CORS_ALLOWED_LIST'),
-    );
-    options.cors = origins;
+    ) as string[];
+    options.cors = {
+      origin: origins,
+      credentials: true,
+    };
 
+    const server = super.createIOServer(port, options);
     server.use(async (socket: AuthenticatedSocket, next) => {
+      // get cookie
       const { cookie: clientCookie } = socket.handshake.headers;
       if (!clientCookie) {
         return next(new Error('Not Authenticated. No cookies were sent'));
       }
 
+      // get accesstoken from cookie
       const { access_token } = cookie.parse(clientCookie);
       if (!access_token) {
         return next(new Error('Not Authenticated'));
       }
 
+      // verify access token
       jwt.verify(
         access_token,
         access_token_public_key,
