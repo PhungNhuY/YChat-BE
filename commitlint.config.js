f1872517125ecb1559d8f9fd2b29207a8138c6dc
PhungNhuY <yphung282@gmail.com>
2025-03-11 10:40:35 +0700

    style: some comments

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,17 +5,17 @@ module.exports = {
       2,
       'always',
       [
-        'feat', // Tính năng mới
-        'fix', // Sửa lỗi
-        'improve', // Cải thiện code
-        'refactor', // Tái cấu trúc code
-        'docs', // Thêm tài liệu
-        'chore', // Thay đổi nhỏ trong quá trình phát triển
-        'style', // Sửa lỗi kiểu chữ, định dạng, không ảnh hưởng đến logic
-        'test', // Viết test
-        'revert', // Revert lại commit trước đó
-        'ci', // Thay đổi cấu hình CI/CD
-        'build', // Build tệp tin
+        'feat', // New feature
+        'fix', // Bug fix
+        'improve', // Code improvement
+        'refactor', // Code refactoring
+        'docs', // Documentation
+        'chore', // Small change in development process
+        'style', // Fix typo, formatting, no logic change
+        'test', // Test code
+        'revert', // Revert previous commit
+        'ci', // CI/CD configuration change
+        'build', // Build file change
       ],
     ],
     'type-case': [2, 'always', 'lower-case'],
